ec90359794d17cbb43b1167790ea2713dcd3910e
RanaM <rana_093@yahoo.com>
2024-05-16 17:30:55 +0600

    Added output.json and completed the basic project

--- imdbscraper/imdbscraper/spiders/imdb_spider.py
+++ imdbscraper/imdbscraper/spiders/imdb_spider.py
@@ -13,11 +13,22 @@ class IMDBSpider(scrapy.Spider):
     top_50_movie_details = response.xpath('//li[contains(@class, "ipc-metadata-list-summary-item")]')[:50]
     print(f'Number of movie items: {len(top_50_movie_details)}')
     for movie_item in top_50_movie_details:
-      movie_name = movie_item.css('.ipc-title__text::text').get().strip()
-      year = movie_item.css('.cli-title-metadata-item::text').get().strip()
+      movie_details_url = movie_item.css('.ipc-title-link-wrapper::attr(href)').get()
+      yield scrapy.Request(url=response.urljoin(movie_details_url), callback=self.parse_movie_details)
 
-      print("Movie Name:", movie_name)
-      print("Year:", year)
-
-  def parse_movie(self, response):
-    pass
+  def parse_movie_details(self, response):
+    movie_name = response.css('span.hero__primary-text::text').get()
+    release_date = response.css('a.ipc-link[href*="releaseinfo"]::text').get()
+    director_name = response.css(
+      'span.ipc-metadata-list-item__label[aria-label="See full cast and crew"] + div ul a::text')\
+      .extract_first()
+    casts = response.css(
+      'a.ipc-metadata-list-item__label[aria-label="See full cast and crew"] + div ul a::text')\
+      .extract()
+    casts = list(set([actor.strip() for actor in casts]))
+    yield {
+      'Movie name': movie_name,
+      'Release date': release_date,
+      'Director': director_name,
+      'casts': casts
+    }
